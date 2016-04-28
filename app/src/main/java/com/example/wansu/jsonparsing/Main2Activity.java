f3a9ab78a823af48cbad8aba1f6bd8ea9b8bf3a3
Wan20 <wandy20number@gmail.com>
2016-04-28 13:07:42 +0700

    JSON Array Example Program

--- app/src/main/java/com/example/wansu/jsonparsing/Main2Activity.java
+++ app/src/main/java/com/example/wansu/jsonparsing/Main2Activity.java
@@ -2,12 +2,53 @@ package com.example.wansu.jsonparsing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Main2Activity extends AppCompatActivity {
-
+    TextView Out;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Out = (TextView) findViewById(R.id.json);
+
+        String strJson=
+                "{ \"Student\" :[" +
+                "{\"id\":\"01\", \"name\":\"Aimbamino\", \"course\":\"Computer Science\", \"ipk\":\"3.9\"}," +
+                "{\"id\":\"02\", \"name\":\"Vimal Jaiswal\", \"course\":\"Computer Engineer\", \"ipk\":\"3.94\"}" +
+                "] }";
+
+        String data = "";
+        try
+        {
+            // Create the root JSONObject from the JSON string.
+            JSONObject jsonRootObject = new JSONObject(strJson);
+
+            //Get the instance of JSONArray that contains JSONObjects
+            JSONArray jsonArray = jsonRootObject.optJSONArray("Student");
+
+            //Iterate the jsonArray and print the info of JSONObjects
+            for(int i=0; i < jsonArray.length(); i++)
+            {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                int id = Integer.parseInt(jsonObject.optString("id").toString());
+                String name = jsonObject.optString("name").toString();
+                String course = jsonObject.optString("course").toString();
+                float ipk = Float.parseFloat(jsonObject.optString("ipk").toString());
+
+                data += " \n\n Id= "+ id +" \n Name= "+ name +" \n Course= "+ course + "\n IPK= "+ ipk +" \n ";
+            }
+            ;
+            Out.setText(data);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/wansu/jsonparsing/MainActivity.java
+++ app/src/main/java/com/example/wansu/jsonparsing/MainActivity.java
@@ -7,57 +7,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    public static final String JSON_STRING="{\"employee\":{\"name\":\"Sachin\", \"course\":\"Computer Science\", \"ipk\":3.94}}";
     Button Button1;
     TextView Out;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button1 = (Button) findViewById(R.id.button);
-        Out = (TextView) findViewById(R.id.json);
+        Out = (TextView)findViewById(R.id.json);
 
-        String strJson="{ \"Student\" :" +
-                "[" +
-                "{\"id\":\"01\",\"name\":\"Aimbamino\",\"course\":\"Computer Science\",\"ipk\":\"3.9\"}," +
-                "{\"id\":\"02\",\"name\":\"Vimal Jaiswal\",\"course\":\"Computer Engineer\",\"ipk\":\"3.94\"}" +
-                "] }";
+        try
+        {
+            JSONObject stu = (new JSONObject(JSON_STRING)).getJSONObject("employee");
+            String name = stu.getString("name");
+            String course = stu.getString("course");
+            int ipk = stu.getInt("ipk");
 
-        String data = "";
-        try {
-            // Create the root JSONObject from the JSON string.
-            JSONObject  jsonRootObject = new JSONObject(strJson);
-
-            //Get the instance of JSONArray that contains JSONObjects
-            JSONArray jsonArray = jsonRootObject.optJSONArray("Student");
-
-            //Iterate the jsonArray and print the info of JSONObjects
-            for(int i=0; i < jsonArray.length(); i++){
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-                int id = Integer.parseInt(jsonObject.optString("id").toString());
-                String name = jsonObject.optString("name").toString();
-                String course = jsonObject.optString("course").toString();
-                float ipk = Float.parseFloat(jsonObject.optString("ipk").toString());
-
-                data += " \n\n Id= "+ id +" \n Name= "+ name +" \n Course= "+ course + "\n IPK= "+ ipk +" \n ";
-            }
-            Out.setText(data);
-        } catch (JSONException e) {e.printStackTrace();}
-
-        Button1.setOnClickListener(new View.OnClickListener(){
+            String str = "\n\n Student" + "\n Name : " + name + "\n Course : " + course + "\n IPK : " + ipk;
+            Out.setText(str);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        Button1.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view){
+            public void onClick(View view)
+            {
                 Intent intent = new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(intent);
             }
         });
     }
 }
-
-
-
